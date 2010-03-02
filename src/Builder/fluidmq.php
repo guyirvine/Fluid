a6c3c6a9cbfa2e63881399586889b87f86c540b2
Guy Irvine <guyirvine@Guy-Irvines-Computer.local>
2010-03-02 19:19:51 +1300

    Minor bug fix

--- src/Builder/fluidmq.php
+++ src/Builder/fluidmq.php
@@ -4,4 +4,4 @@ if ( $_SERVER['argc'] < 2 )
 	die( "fluidbuilder fluidmq\n" );
 
 
-write_file( "./src/FluidMq.php", get_content( getAbsoluteFluidTemplateDirectory() . "/FluidMq.php", $name ) );
+write_file( "./src/FluidMq.php", file_get_contents( getAbsoluteFluidTemplateDirectory() . "/FluidMq.php" ) );
